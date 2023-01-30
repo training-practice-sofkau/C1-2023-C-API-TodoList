f2ac1b9dcdd1a999818abb303801912dba43becd
David Fernando Briceño Galindo <d.bricenogalindo@gmail.com>
2023-01-30 15:29:39 -0500

    Configuracion de mapeo

--- TodoListSofka/Program.cs
+++ TodoListSofka/Program.cs
@@ -4,8 +4,8 @@ using TodoListSofka.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
