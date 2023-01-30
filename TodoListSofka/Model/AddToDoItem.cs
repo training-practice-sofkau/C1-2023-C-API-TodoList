2c99ea3fba13183cc36a85d00a5eb4cae70d0eb6
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-30 12:42:46 -0500

    Adding AddToDoItem Model
    For the get request

---
+++ TodoListSofka/Model/AddToDoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoListSofka.Model
+{
+    public class AddToDoItem
+    {
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Responsible { get; set; } = null!;
+    }
+}
