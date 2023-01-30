2ded5abcd2ac6cce50f1ad76cf10ee0bd56035fe
Kevin Baquero <baquerochavarro@gmail.com>
2023-01-30 12:43:14 -0500

    Adding UpdateToDoItem Model
    For the put request

---
+++ TodoListSofka/Model/UpdateToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListSofka.Model
+{
+    public class UpdateToDoItem
+    {
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Responsible { get; set; } = null!;
+
+        public bool IsCompleted { get; set; }
+    }
+}
